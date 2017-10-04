b95ff497db16555428c2435c779a5d4e19b4106e
devin-petersohn <devin.petersohn@gmail.com>
2017-10-04 00:39:20 -0700

    Adding reads and features to the toy graph

--- g_graph.py
+++ g_graph.py
@@ -4,18 +4,18 @@ import ray.local_scheduler as local_scheduler
 # store access to nodes by their global coordinate
 class MasterStore:
     referenceStore = {"dna":{}, "rna":{}, "individuals":{}}
-    
+
     def addGraph(self, datatype):
-        referenceStore[datatype] = {}
-    
+        self.referenceStore[datatype] = {}
+
     def updateRef(self, datatype, key, value):
         oid = value
-        
+
         if not isinstance(oid, local_scheduler.ObjectID):
             oid = ray.put(value)        
 
         self.referenceStore[datatype][key] = oid
-        
+
     def getRef(self, datatype, key):
         ref = self.referenceStore[datatype][key]
         # check if this is a ray object id or a node
@@ -25,7 +25,7 @@ class MasterStore:
             return ref
         else:
             raise ValueError("The graph does not contain references or nodes at key: \'" + key + "\'")
-    
+
     def refExists(self, datatype, key):
         return datatype in self.referenceStore and key in self.referenceStore[datatype]
 
@@ -36,10 +36,10 @@ class Node:
         self.datatype = datatype
         self.neighbors = neighbors
         self.interGraphLinks = {}
-        
+
     def addNeighbor(self, newNeighbor):
         self.neighbors.add(newNeighbor)
-        
+
     def dropNeighbor(self, oldNeighbor):
         self.neighbors.remove(oldNeighbor)
 
@@ -49,12 +49,11 @@ class Node:
                 self.interGraphLinks[datatype].append(key)
         else:
             self.interGraphLinks[datatype] = [key]
-        
 
     def merge(self, otherNode):
         for i in otherNode.neighbors:
             self.addNeighbor(i)
-        
+
         for key, value in otherNode.interGraphLinks.items():
             for link in value:
                 self.addInterGraphLink(key, link)
@@ -89,20 +88,20 @@ def buildDnaGraph(referenceGenome, dnaTestData, masterStore):
                         neighbors)
 
             node.addInterGraphLink("individuals", indiv["individualID"])
-            
-            if(masterStore.refExists("dna", variant["coordinateStart"])):
+
+            if masterStore.refExists("dna", variant["coordinateStart"]):
                 node.merge(masterStore.getRef("dna", variant["coordinateStart"]))
 
             for neighbor in neighbors:
-                if(masterStore.refExists("dna", neighbor)):
+                if masterStore.refExists("dna", neighbor):
                     tempNode = masterStore.getRef("dna", neighbor)
                     tempNode.addNeighbor(variant["coordinateStart"])
                     masterStore.updateRef("dna", neighbor, tempNode)
-                    
+
             indivNode = masterStore.getRef("individuals", indiv["individualID"])
             indivNode.addInterGraphLink("dna", variant["coordinateStart"])
             masterStore.updateRef("individuals", indiv["individualID"], indivNode)
-            
+
             masterStore.updateRef("dna", variant["coordinateStart"], node)
 
 def bfs(graphName, startNodeID, masterStore):
@@ -113,10 +112,10 @@ def bfs(graphName, startNodeID, masterStore):
         currentKey = q[0]
         q.remove(currentKey)
         visited.append(currentKey)
-        
+
         node = masterStore.getRef(graphName, currentKey)
         print(str(currentKey) + "\t" + str(node.data) + "\t" + str(node.interGraphLinks))
-        
+
         for i in node.neighbors:
             if i not in visited and i not in q:
                 q.append(i)
@@ -154,3 +153,91 @@ if __name__ == "__main__":
 
     print(masterStore.getRef("individuals", 0))
     print(masterStore.getRef("individuals", 1))
+
+    # this will store all reads in their original form
+    masterStore.addGraph("reads")
+    # this will store the genome graph for all reads
+    masterStore.addGraph("readsGenomeGraph")
+
+    #sample reads
+    sampleReadData = [{"contigName": "chr1", "start": 268051, "end": 268101, "mapq": 0, "readName": "D3NH4HQ1:95:D0MT5ACXX:2:2307:5603:121126", "sequence": "GGAGTGGGGGCAGCTACGTCCTCTCTTGAGCTACAGCAGATTCACTCNCT", "qual": "BCCFDDFFHHHHHJJJIJJJJJJIIIJIGJJJJJJJJJIIJJJJIJJ###", "cigar": "50M", "readPaired": False, "properPair": False, "readMapped": True, "mateMapped": False, "failedVendorQualityChecks": False, "duplicateRead": False, "readNegativeStrand": False, "mateNegativeStrand": False, "primaryAlignment": True, "secondaryAlignment": False, "supplementaryAlignment": False, "mismatchingPositions": "47T0G1", "origQual": None, "attributes": "XT:A:R\tXO:i:0\tXM:i:2\tNM:i:2\tXG:i:0\tXA:Z:chr16,-90215399,50M,2;chr6,-170736451,50M,2;chr8,+71177,50M,3;chr1,+586206,50M,3;chr1,+357434,50M,3;chr5,-181462910,50M,3;chr17,-83229095,50M,3;\tX1:i:5\tX0:i:3", "recordGroupName": None, "recordGroupSample": None, "mateAlignmentStart": None, "mateAlignmentEnd": None, "mateContigName": None, "inferredInsertSize": None},
+                        {"contigName": "chr1", "start": 1424219, "end": 1424269, "mapq": 37, "readName": "D3NH4HQ1:95:D0MT5ACXX:2:2107:15569:102571", "sequence": "AGCGCTGTAGGGACACTGCAGGGAGGCCTCTGCTGCCCTGCTAGATGTCA", "qual": "CCCFFFFFHHHHHJJJJJJJJJJIJJJJJJJJJJJJJJJIJJIJHIIGHI", "cigar": "50M", "readPaired": False, "properPair": False, "readMapped": True, "mateMapped": False, "failedVendorQualityChecks": False, "duplicateRead": False, "readNegativeStrand": False, "mateNegativeStrand": False, "primaryAlignment": True, "secondaryAlignment": False, "supplementaryAlignment": False, "mismatchingPositions": "50", "origQual": None, "attributes": "XT:A:U\tXO:i:0\tXM:i:0\tNM:i:0\tXG:i:0\tX1:i:0\tX0:i:1", "recordGroupName": None, "recordGroupSample": None, "mateAlignmentStart": None, "mateAlignmentEnd": None, "mateContigName": None, "inferredInsertSize": None},
+                        {"contigName": "chr1", "start": 1443674, "end": 1443724, "mapq": 0, "readName": "D3NH4HQ1:95:D0MT5ACXX:2:2103:19714:5712", "sequence": "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT", "qual": "############################BBBCDEEA<?:FDCADDD?;=+", "cigar": "50M", "readPaired": False, "properPair": False, "readMapped": True, "mateMapped": False, "failedVendorQualityChecks": False, "duplicateRead": False, "readNegativeStrand": True, "mateNegativeStrand": False, "primaryAlignment": True, "secondaryAlignment": False, "supplementaryAlignment": False, "mismatchingPositions": "50", "origQual": None, "attributes": "XT:A:R\tXO:i:0\tXM:i:0\tNM:i:0\tXG:i:0\tX0:i:1406", "recordGroupName": None, "recordGroupSample": None, "mateAlignmentStart": None, "mateAlignmentEnd": None, "mateContigName": None, "inferredInsertSize": None},
+                        {"contigName": "chr1", "start": 1443676, "end": 1443726, "mapq": 0, "readName": "D3NH4HQ1:95:D0MT5ACXX:2:2103:21028:126413", "sequence": "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT", "qual": "###########################B?;;AFHFIGDDHDDDDDBD@?=", "cigar": "50M", "readPaired": False, "properPair": False, "readMapped": True, "mateMapped": False, "failedVendorQualityChecks": False, "duplicateRead": False, "readNegativeStrand": True, "mateNegativeStrand": False, "primaryAlignment": True, "secondaryAlignment": False, "supplementaryAlignment": False, "mismatchingPositions": "50", "origQual": None, "attributes": "XT:A:R\tXO:i:0\tXM:i:0\tNM:i:0\tXG:i:0\tX0:i:1406", "recordGroupName": None, "recordGroupSample": None, "mateAlignmentStart": None, "mateAlignmentEnd": None, "mateContigName": None, "inferredInsertSize": None},
+                        {"contigName": "chr1", "start": 2653642, "end": 2653692, "mapq": 25, "readName": "D3NH4HQ1:95:D0MT5ACXX:2:2306:20003:84408", "sequence": "ANNACACCCCCAGGCGAGCATCTGACAGCCTGGAACAGCACCCACACCCC", "qual": "######JJJJJJJIJIJJIHGGGIIJJJJJJJJJJJJHHFHHDDDBFC@@", "cigar": "50M", "readPaired": False, "properPair": False, "readMapped": True, "mateMapped": False, "failedVendorQualityChecks": False, "duplicateRead": False, "readNegativeStrand": True, "mateNegativeStrand": False, "primaryAlignment": True, "secondaryAlignment": False, "supplementaryAlignment": False, "mismatchingPositions": "0T0C0C47", "origQual": None, "attributes": "XT:A:U\tXO:i:0\tXM:i:3\tNM:i:3\tXG:i:0\tX1:i:0\tX0:i:1", "recordGroupName": None, "recordGroupSample": None, "mateAlignmentStart": None, "mateAlignmentEnd": None, "mateContigName": None, "inferredInsertSize": None},
+                        {"contigName": "chr1", "start": 2664732, "end": 2664782, "mapq": 0, "readName": "D3NH4HQ1:95:D0MT5ACXX:2:2106:12935:169714", "sequence": "GAGCATGTGACAGCCTAGGTCGGCACCCACACCCCCAGGTGAGCATCTGA", "qual": "FDBDCHFFEHDCCAFHIHA6EGB?8GGFF?8IEHEB@FHDHGEDDBD@@@", "cigar": "50M", "readPaired": False, "properPair": False, "readMapped": True, "mateMapped": False, "failedVendorQualityChecks": False, "duplicateRead": False, "readNegativeStrand": True, "mateNegativeStrand": False, "primaryAlignment": True, "secondaryAlignment": False, "supplementaryAlignment": False, "mismatchingPositions": "6C9G33", "origQual": None, "attributes": "XT:A:R\tXO:i:0\tXM:i:2\tNM:i:2\tXG:i:0\tX1:i:13\tX0:i:5", "recordGroupName": None, "recordGroupSample": None, "mateAlignmentStart": None, "mateAlignmentEnd": None, "mateContigName": None, "inferredInsertSize": None},
+                        {"contigName": "chr1", "start": 2683541, "end": 2683591, "mapq": 0, "readName": "D3NH4HQ1:95:D0MT5ACXX:2:2107:5053:12847", "sequence": "AGCACCCACAACCACAGGTGAGCATCCGACAGCCTGGAACAGCACCCACA", "qual": "CCCFFFFFHGHHHJIJJJHGGIIJJJJJIJGIIJJIJJIJJJJIJIIJJJ", "cigar": "50M", "readPaired": False, "properPair": False, "readMapped": True, "mateMapped": False, "failedVendorQualityChecks": False, "duplicateRead": False, "readNegativeStrand": False, "mateNegativeStrand": False, "primaryAlignment": True, "secondaryAlignment": False, "supplementaryAlignment": False, "mismatchingPositions": "50", "origQual": None, "attributes": "XT:A:R\tXO:i:0\tXM:i:0\tNM:i:0\tXG:i:0\tXA:Z:chr1,+2687435,50M,0;chr1,+2694861,50M,0;chr1,+2755813,50M,1;\tX1:i:1\tX0:i:3", "recordGroupName": None, "recordGroupSample": None, "mateAlignmentStart": None, "mateAlignmentEnd": None, "mateContigName": None, "inferredInsertSize": None},
+                        {"contigName": "chr1", "start": 2689861, "end": 2689911, "mapq": 0, "readName": "D3NH4HQ1:95:D0MT5ACXX:2:2108:5080:115408", "sequence": "GGTGAGCATCTGACAGCCCGGAGCAGCACGCAAACCCCCAGGTGAGCATC", "qual": "@@BFBBDFHHHHGJIJGIIFIEIJJJJIJJJJJJJJJJJJIJGHHICEHH", "cigar": "50M", "readPaired": False, "properPair": False, "readMapped": True, "mateMapped": False, "failedVendorQualityChecks": False, "duplicateRead": False, "readNegativeStrand": False, "mateNegativeStrand": False, "primaryAlignment": True, "secondaryAlignment": False, "supplementaryAlignment": False, "mismatchingPositions": "18T3A27", "origQual": None, "attributes": "XT:A:R\tXO:i:0\tXM:i:2\tNM:i:2\tXG:i:0\tX1:i:21\tX0:i:2", "recordGroupName": None, "recordGroupSample": None, "mateAlignmentStart": None, "mateAlignmentEnd": None, "mateContigName": None, "inferredInsertSize": None},
+                        {"contigName": "chr1", "start": 2750194, "end": 2750244, "mapq": 0, "readName": "D3NH4HQ1:95:D0MT5ACXX:2:1204:10966:151563", "sequence": "CCCCCNCACCCCCAGGTGAGCATCTGATGGTCTGGAGCAGCACCCACACC", "qual": "######F;JJJJJJJJJJJJIIIJIJJJJFJJIJJGJHHHHHFFDDD?BB", "cigar": "50M", "readPaired": False, "properPair": False, "readMapped": True, "mateMapped": False, "failedVendorQualityChecks": False, "duplicateRead": False, "readNegativeStrand": True, "mateNegativeStrand": False, "primaryAlignment": True, "secondaryAlignment": False, "supplementaryAlignment": False, "mismatchingPositions": "1A3A12C31", "origQual": None, "attributes": "XT:A:R\tXO:i:0\tXM:i:3\tNM:i:3\tXG:i:0\tXA:Z:chr1,-2653118,50M,3;chr1,-2652838,50M,3;chr1,-2653681,50M,3;chr1,-2694823,50M,3;chr1,-2687397,50M,3;chr1,-2755775,50M,3;chr1,-2653921,50M,3;\tX1:i:0\tX0:i:8", "recordGroupName": None, "recordGroupSample": None, "mateAlignmentStart": None, "mateAlignmentEnd": None, "mateContigName": None, "inferredInsertSize": None},
+                        {"contigName": "chr1", "start": 3052271, "end": 3052321, "mapq": 25, "readName": "D3NH4HQ1:95:D0MT5ACXX:2:2107:21352:43370", "sequence": "TCANTCATCTTCCATCCATCCGTCCAACAACCATTTGTTGATCATCTCTC", "qual": "@@<#4AD?ACDCDHGIDA>C?<A;8CBEEBAG1D?BG?GH?@DEHFG@FH", "cigar": "50M", "readPaired": False, "properPair": False, "readMapped": True, "mateMapped": False, "failedVendorQualityChecks": False, "duplicateRead": False, "readNegativeStrand": False, "mateNegativeStrand": False, "primaryAlignment": True, "secondaryAlignment": False, "supplementaryAlignment": False, "mismatchingPositions": "3C44A0T0", "origQual": None, "attributes": "XT:A:U\tXO:i:0\tXM:i:3\tNM:i:3\tXG:i:0\tX1:i:0\tX0:i:1", "recordGroupName": None, "recordGroupSample": None, "mateAlignmentStart": None, "mateAlignmentEnd": None, "mateContigName": None, "inferredInsertSize": None}]
+
+    for read in sampleReadData:
+        masterStore.updateRef("reads", read["readName"], read)
+        for index in range(len(read["sequence"])):
+            data = [read["readName"]]
+
+            neighbors = set()
+            if index != 0:
+                neighbors.add(read["contigName"] + "\t" + str(read["start"] + index - 1))
+            if index != len(read["sequence"]) - 1:
+                neighbors.add(read["contigName"] + "\t" + str(read["start"] + index + 1))
+
+            node = Node(data, "readsGenomeGraph", neighbors)
+            # in order to connect to DNA we need a way to globally link the two via coordinate systems
+            #node.addInterGraphLink("dna", location)
+
+            coordinate = read["contigName"] + "\t" + str(read["start"] + index)
+
+            if masterStore.refExists("readsGenomeGraph", coordinate):
+                previousNode = masterStore.getRef("readsGenomeGraph", coordinate)
+                node.merge(previousNode)
+                node.data += previousNode.data
+
+            node.addInterGraphLink("reads", read["readName"])
+            masterStore.updateRef("readsGenomeGraph", coordinate, node)
+
+    print(len(masterStore.referenceStore["reads"]))
+    print(len(masterStore.referenceStore["readsGenomeGraph"]))
+
+    sampleFeatures = [{"featureName": "0","contigName": "chr1", "start": 45520936, "end": 45522463, "score": 0.0, "attributes": {"itemRgb": "5.0696939910406", "blockCount": "878", "thickStart": "482.182760214932", "thickEnd": "-1"}},
+                        {"featureName": "1", "contigName": "chr1", "start": 88891087, "end": 88891875, "score": 0.0, "attributes": {"itemRgb": "5.0696939910406", "blockCount": "423", "thickStart": "446.01797654123", "thickEnd": "-1"}},
+                        {"featureName": "2", "contigName": "chr1", "start": 181088138, "end": 181090451, "score": 0.0, "attributes": {"itemRgb": "5.0696939910406", "blockCount": "626", "thickStart": "444.771802710521", "thickEnd": "-1"}},
+                        {"featureName": "3", "contigName": "chr1", "start": 179954184, "end": 179955452, "score": 0.0, "attributes": {"itemRgb": "5.0696939910406", "blockCount": "647", "thickStart": "440.10466093652", "thickEnd": "-1"}},
+                        {"featureName": "4", "contigName": "chr1", "start": 246931401, "end": 246932507, "score": 0.0, "attributes": {"itemRgb": "5.0696939910406", "blockCount": "423", "thickStart": "436.374938660247", "thickEnd": "-1"}},
+                        {"featureName": "5", "contigName": "chr1", "start": 28580676, "end": 28582443, "score": 0.0, "attributes": {"itemRgb": "5.0696939910406", "blockCount": "1106", "thickStart": "434.111845970505", "thickEnd": "-1"}},
+                        {"featureName": "6", "contigName": "chr1", "start": 23691459, "end": 23692369, "score": 0.0, "attributes": {"itemRgb": "5.0696939910406", "blockCount": "421", "thickStart": "426.055504846001", "thickEnd": "-1"}},
+                        {"featureName": "7", "contigName": "chr1", "start": 201955033, "end": 201956082, "score": 0.0, "attributes": {"itemRgb": "5.0696939910406", "blockCount": "522", "thickStart": "423.882565088207", "thickEnd": "-1"}},
+                        {"featureName": "8", "contigName": "chr1", "start": 207321011, "end": 207323021, "score": 0.0, "attributes": {"itemRgb": "5.0696939910406", "blockCount": "741", "thickStart": "423.625988483304", "thickEnd": "-1"}},
+                        {"featureName": "9", "contigName": "chr1", "start": 110963118, "end": 110964762, "score": 0.0, "attributes": {"itemRgb": "5.0696939910406", "blockCount": "758", "thickStart": "421.056761458099", "thickEnd": "-1"}}]
+
+    # for storing the feature data
+    masterStore.addGraph("features")
+    # for storing the feature genome graph
+    masterStore.addGraph("featuresGenomeGraph")
+
+    for feature in sampleFeatures:
+        masterStore.updateRef("features", feature["featureName"], feature)
+        for index in range(feature["end"] - feature["start"]):
+            data = [feature["featureName"]]
+            coordinate = feature["contigName"] + "\t" + str(read["start"] + index)
+
+            neighbors = set()
+            if index != 0:
+                neighbors.add(feature["contigName"] + "\t" + str(read["start"] + index - 1))
+            if index != (feature["end"] - feature["start"] - 1):
+                neighbors.add(feature["contigName"] + "\t" + str(read["start"] + index + 1))
+
+            node = Node(data, "featuresGenomeGraph", neighbors)
+            # in order to connect to DNA we need a way to globally link the two via coordinate systems
+            #node.addInterGraphLink("dna", location)
+
+            if masterStore.refExists("featuresGenomeGraph", coordinate):
+                previousNode = masterStore.getRef("featuresGenomeGraph", coordinate)
+                node.merge(previousNode)
+                node.data += previousNode.data
+
+            node.addInterGraphLink("features", feature["featureName"])
+            masterStore.updateRef("featuresGenomeGraph", coordinate, node)
+
+    print(len(masterStore.referenceStore["features"]))
+    print(len(masterStore.referenceStore["featuresGenomeGraph"]))
